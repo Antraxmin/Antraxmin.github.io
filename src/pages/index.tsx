a67b190c46ac7dbd46db95f2dd1b55d80e2f9191
Antraxmin <antraxmin@naver.com>
2024-06-30 13:01:46 +0900

    [fix] GraphQL 쿼리 결과를 위한 인터페이스 정의

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,21 @@ import { PageProps, graphql } from 'gatsby'
 import Layout from '../components/common/Layout'
 import React from 'react'
 
+interface IndexPageQuery {
+  allContentfulPost: {
+    nodes: {
+      title: string
+      slug: string
+      date: string
+    }[]
+  }
+}
+
 export default function Index({
   data: {
     allContentfulPost: { nodes },
   },
-}: PageProps<Queries.IndexPageQuery>) {
+}: PageProps<IndexPageQuery>) {
   return (
     <Layout>
       {nodes.map(({ title, slug, date }) => (

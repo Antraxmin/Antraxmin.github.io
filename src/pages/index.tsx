9f5e84ea820dd07e25d1640f5e40b6a5f617e9be
Antraxmin <antraxmin@naver.com>
2024-06-30 19:48:55 +0900

    [feat] 카테고리 필터링 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,38 @@
 import { PageProps, graphql } from 'gatsby'
-import React from 'react'
-import Layout from '../components/common/Layout'
+import React, { useState } from 'react'
 import Introduction from '../components/main/Introduction'
+import Category from '../components/main/Category'
 
 export default function Index({
   data: {
     allContentfulPost: { nodes },
   },
 }: PageProps<Queries.IndexPageQuery>) {
+  const [selectedCategory, setSelectedCategory] = useState<string>('All')
+  const categories = nodes.reduce<Record<string, number>>(
+    (categories, post) => {
+      post.category
+        ?.filter((category): category is string => !!category)
+        .forEach(
+          category => (categories[category] = (categories[category] ?? 0) + 1),
+        )
+
+      return categories
+    },
+    { All: nodes.length },
+  )
+
+  const handleSelectCategory = (category: string) =>
+    setSelectedCategory(category)
+
   return (
     <>
       <Introduction />
+      <Category
+        categories={categories}
+        selectedCategory={selectedCategory}
+        handleSelect={handleSelectCategory}
+      />
       {nodes.map(({ title, slug, date }) => (
         <div key={slug}>
           {title} / {date} / {slug}
@@ -25,6 +47,7 @@ export const query = graphql`
     allContentfulPost(sort: { date: DESC }) {
       nodes {
         title
+        category
         slug
         date
       }

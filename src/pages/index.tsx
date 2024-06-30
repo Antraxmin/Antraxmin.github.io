a4aab3e15b4e38579c1b0ff02a315a0e24f4c004
Antraxmin <antraxmin@naver.com>
2024-06-30 14:59:32 +0900

    [feat] 메인페이지 intro 배너 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { PageProps, graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../components/common/Layout'
+import Introduction from '../components/main/Introduction'
 
 export default function Index({
   data: {
@@ -9,6 +10,7 @@ export default function Index({
 }: PageProps<Queries.IndexPageQuery>) {
   return (
     <>
+      <Introduction />
       {nodes.map(({ title, slug, date }) => (
         <div key={slug}>
           {title} / {date} / {slug}
